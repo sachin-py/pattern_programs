bda25f1118308fa06c9cbbbf711b437da8a2b0a7
sachin-py <sy6080@gmail.com>
2025-06-18 21:14:17 +0530

    added righthalfdiamond pattern

--- rightHalfDiamondFixDigitinRowPattern.py
+++ rightHalfDiamondFixDigitinRowPattern.py
@@ -8,10 +8,23 @@
 3 3 3
 2 2
 1
+
+This program prints a pyramid pattern of numbers:
+- The upper half increases from 1 to n, with each number repeated on its line.
+- The lower half is a mirror of the upper half in reverse.
 '''
 
+# Author : Sachin Kumar
+# GitHub : https://github.com/sachin-py/pattern_programs/
+
 n = 5
+
+# Upper half of the pattern (1 to n)
 for i in range(n):
-    print((str(i+1)+' ')*(i+1))
+    # Print the number (i+1), repeated (i+1) times with space
+    print((str(i+1) + ' ') * (i+1))
+
+# Lower half of the pattern (n-1 to 1)
 for i in range(n-1):
-    print((str(n-i-1)+' ')*(n-i-1))
+    # Print the number (n - i - 1), repeated that many times
+    print((str(n - i - 1) + ' ') * (n - i - 1))
